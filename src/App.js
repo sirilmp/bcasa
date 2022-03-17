524b4a12bb783e743ab3bc152fd324b916cdac1f
sirilmp <sirilmp8@gmail.com>
2022-03-17 07:44:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [show, setShow] = useState(false)
+
+  const toggleOnClick=()=>{
+    const toggle=document.getElementById('toggle')
+    toggle.classList.toggle('active')
+    setShow(!show)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='wrapper'>
+      <div className="holder">
+        <div className="candle">
+          <div className={`${show&&'blinking-glow'}`}></div>
+          <div className={`thread`}></div>
+          <div className={`${show&&'glow'} `}></div>
+          <div className={`${show&&'flame'}`}></div>
+        </div>
+      </div>
+      <div id="toggle" >
+        <p onClick={toggleOnClick} className={`indicator`}></p>
+    </div> 
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
